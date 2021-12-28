67932d8dd6b046952402caf4ad66b6bdbb788cef
Olimjon <Gofuroov@gmail.com>
2021-12-28 11:22:32 +0500

    Configure api module #1

--- api/controllers/BaseController.php
+++ api/controllers/BaseController.php
@@ -8,25 +8,28 @@
 namespace api\controllers;
 
 use app\config\Cors;
+use yii\web\Response;
 
 class BaseController extends \yii\rest\Controller
 {
     /**
      * @inheritDoc
      */
-    public function behaviors()
+    public function behaviors(): array
     {
         $behaviors = parent::behaviors();
 
         // remove authentication filter
         $auth = $behaviors['authenticator'];
         unset($behaviors['authenticator']);
-
         // add CORS filter
         $behaviors['corsFilter'] = Cors::settings();
-
         // re-add authentication filter
         $behaviors['authenticator'] = $auth;
+        // content Negotiator
+        $behaviors['contentNegotiator']['formats'] = [
+            'application/json' => Response::FORMAT_JSON,
+        ];
         // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
         $behaviors['authenticator']['except'] = ['options'];
 
