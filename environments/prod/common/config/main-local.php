1eba683a4c271fc98ed66a0c58cd5c5e35909684
Olimjon <gofuroov@gmail.com>
2022-07-11 16:41:53 +0500

    fix env local-main

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -6,7 +6,7 @@ return [
     'components' => [
         'db' => [
             'class' => Connection::class,
-            'dsn' => 'mysql:host=' . $_ENV['DB_HOST'] ?? 'localhost' . ';dbname=' . $_ENV['DB_NAME'],
+            'dsn' => 'mysql:host=' . ($_ENV['DB_HOST'] ?? 'localhost') . ';dbname=' . $_ENV['DB_NAME'],
             'username' => $_ENV['DB_USER'] ?? 'root',
             'password' => $_ENV['DB_PASS'] ?? '',
             'charset' => $_ENV['DB_CHARSET'] ?? 'utf8',
